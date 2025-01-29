c234cdc86d64b59f9cc8c9c6d56fa1e7d730e57a
Zdzislaw <zdzislaw22@windowslive.com>
2025-01-29 09:47:45 +0100

    clean EK

--- EmuKodi/EmuKodi/cdmplayerPatcher.sh
+++
@@ -1,9 +0,0 @@
-find /usr/lib/enigma2/python/Plugins/Extensions/EmuKodi/site-packages/cdmplayer* -iname *.py | 
-  while read F 
-  do
-    #echo "$F"
-    sed "s/streamlink\./cdmplayer./g" -i "$F"
-    sed "s/streamlink_cli\./cdmplayer_cli./g" -i "$F"
-        sed "s/from streamlink import/from cdmplayer import/g" -i "$F"
-        sed "s/from streamlink_cli import/from cdmplayer_cli import/g" -i "$F"
-  done
